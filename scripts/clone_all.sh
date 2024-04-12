2d0ad47a7639a112ab923e8c30feda9fffef4208
Arnav Nidumolu <arnav.nidumolu@gmail.com>
2024-04-12 15:33:24 -0400

    fixed cloning script

--- scripts/clone_all.sh
+++ scripts/clone_all.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
 gh repo list CS520-news-aggregator --limit 4000 | while read -r repo _; do
+    if [ "$repo" == "CS520-news-aggregator/news-aggregator" ]; then
+        continue
+    fi
+    echo "Cloning $repo"
     gh repo clone "$repo"
 done
