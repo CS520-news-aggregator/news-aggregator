a81dbbfda1bdcfd7dd2e7b94059137da9942f880
Arnav Nidumolu <arnav.nidumolu@gmail.com>
2024-04-23 03:20:56 -0400

    bacj

--- scripts/clone_all.sh
+++ scripts/clone_all.sh
@@ -5,5 +5,5 @@ gh repo list CS520-news-aggregator --limit 4000 | while read -r repo _; do
         continue
     fi
     echo "Cloning $repo"
-    git clone --recursive "$repo"
+    gh repo clone "$repo"
 done
